61bc837d1288091b0e154703736f76830d9804a8
YevgenySemak <48668000+euseand@users.noreply.github.com>
2021-06-08 16:47:02 +0300

    feat: added day 7 task

---
+++ day_7/hangman.py
@@ -0,0 +1,34 @@
+from random import choice
+
+from constants import STAGES, WORD_LIST
+
+
+word = choice(WORD_LIST)
+blanks = list(['_' for _ in word])
+
+lives = 6
+while '_' in blanks and lives > 0:
+    print(''.join(blanks))
+    print(STAGES[lives])
+    guess = input('Guess a letter (0 to exit): ')
+    print('----------------------------------------------------------------')
+
+    if guess == '0':
+        break
+
+    if guess in blanks:
+        print(f'You have already guessed {guess}. You want to die or what?\n')
+
+    if guess in word:
+        for idx, letter in enumerate(word):
+            if letter == guess:
+                blanks[idx] = letter
+    else:
+        lives -= 1
+        print(f'You guessed {guess}. It is not in a word.\n')
+   
+if lives == 0:
+    print(STAGES[lives])
+    print(f'You have lost. It was the word "{word}"')
+else:
+    print(f'You have won. It was the word "{word}"')
