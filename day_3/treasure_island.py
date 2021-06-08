f215a11e6ed3afd998068582ddc210e18b6708b2
YevgenySemak <48668000+euseand@users.noreply.github.com>
2021-06-08 14:25:22 +0300

    fix: fixed code style issues

--- day_3/treasure_island.py
+++ day_3/treasure_island.py
@@ -9,7 +9,7 @@ print('''
 |                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
 |___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
           |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+ _________|___________| ;`-.o`"=._; ." ` '`."/` . "-._ /_______________|_______
 |                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
 |___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
 ____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
@@ -22,16 +22,18 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print('Weclome to Treasure Island.\nYour mission is to find the treasure.')
 turn = input('You are at a cross road. Where do you want to go? Type "left" or "right"\n').lower()
-if turn == 'left':    
-    lake = input('You come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
+if turn == 'left':
+    lake = input('You come to a lake. There is an island in the middle of the lake. \
+        Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
     if lake == 'wait':
-        doors = input('You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?\n').lower()
+        doors = input('You arrive at the island unharmed. There is a house with 3 doors. \
+            One red, one yellow and one blue. Which color do you choose?\n').lower()
         if doors == 'red':
             print('It is a room full of fire. Game Over.')
         elif doors == 'yellow':
             print('You win. Gold is yours.')
         elif doors == 'blue':
-                print('Eaten by beasts. Game Over.')
+            print('Eaten by beasts. Game Over.')
     elif lake == 'swim':
         print('You have been attacked by a trout. Game Over.')
 elif turn == 'right':
