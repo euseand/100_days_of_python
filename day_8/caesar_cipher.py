fb9628ae6a5d58afb0115570b29c5e160ca9c554
YevgenySemak <48668000+euseand@users.noreply.github.com>
2021-06-08 17:38:09 +0300

    feat: day 8 task

---
+++ day_8/caesar_cipher.py
@@ -0,0 +1,32 @@
+from utils import clear_message, caesar
+
+
+while True:
+
+    while True:
+        action = input('Type "encode" to encrypt, type "decode" to decrypt:\n').lower()
+        if action not in {'encode', 'decode'}:
+            print('Unnknown action. Please try again.\n')
+            continue
+        break
+
+    message = list((input('\nType your message:\n').lower()))
+    message = clear_message(message)
+    print(f'Everything but chars has been removed from message: {"".join(message)}\n')
+
+    while True:
+        try:
+            shift = int(input('\nType the shift number:\n'))
+        except ValueError:
+            print('It is not valid integer. Please try again.\n')
+            continue
+        break
+
+    if action == 'encode':
+        print(f'\nHere is the encoded result:\n{caesar(action, message, shift)}')
+    elif action == 'decode':
+        print(f'\nHere is the decoded result:\n{caesar(action, message, shift)}')
+
+    choice = input('Type "yes" if you want to go again. Otherwise type "no".\n').lower()
+    if choice == 'no':
+        break
---
+++ day_8/utils.py
@@ -0,0 +1,19 @@
+from string import ascii_lowercase
+
+
+def caesar(action, message, shift):        
+        if action == 'encode':
+            for idx, letter in enumerate(message):
+                pos = ascii_lowercase.index(letter)
+                new_pos = (pos + shift) % len(ascii_lowercase)
+                message[idx] = ascii_lowercase[new_pos]
+        elif action == 'decode':
+            for idx, letter in enumerate(message):
+                pos = ascii_lowercase.index(letter)
+                new_pos = (pos - shift) % len(ascii_lowercase)
+                message[idx] = ascii_lowercase[new_pos]
+        return ''.join(message)
+
+
+def clear_message(message):
+    return list([char for char in message if char in ascii_lowercase])
