d4f37b530275e8d9300b1610a7d38bfef2dec24a
YevgenySemak <48668000+euseand@users.noreply.github.com>
2021-06-14 11:08:49 +0300

    feat: finished day 23 task

--- day_023/car_manager.py
+++ day_023/car_manager.py
@@ -1,29 +1,38 @@
 from turtle import Turtle
 from random import randint
 
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, CAR_MOVE_DISTANCE
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, CAR_MOVE_DISTANCE, CAR_MOVE_INCREMENT
 
 
 class CarManager:
     def __init__(self):
         self.cars = []
-        for _ in range(100):
-            self.create_car()
-            self.move_speed = 0.1
+        #for _ in range(100):
+        #    self.create_car()
+        self.move_speed = 0.1
+        self.move_distance = CAR_MOVE_DISTANCE
 
     def create_car(self):
-        car = Turtle()
-        car.shape('square')
-        car.penup()
-        car.shapesize(stretch_len=2, stretch_wid=1)
-        car.setheading(180)
-        random_color = (randint(1, 255), randint(1, 255), randint(1, 255))
-        car.color(random_color)
-        random_position = (randint(-SCREEN_WIDTH/2, SCREEN_WIDTH/2+50),
-                           randint(-SCREEN_HEIGHT/2+70, SCREEN_HEIGHT/2-70))
-        car.goto(random_position)
-        self.cars.append(car)
+        random_chance = randint(1, 6)
+        if random_chance == 1:
+            car = Turtle('square')
+            car.penup()
+            car.shapesize(stretch_len=2, stretch_wid=1)
+            car.setheading(180)
+            random_color = (randint(1, 255), randint(1, 255), randint(1, 255))
+            car.color(random_color)
+            rand_x = randint(SCREEN_WIDTH/2, SCREEN_WIDTH/2+50)
+            rand_y = randint(-SCREEN_HEIGHT/2+70, SCREEN_HEIGHT/2-70)
+            car.goto(rand_x, rand_y)
+            self.cars.append(car)
 
     def move(self):
         for car in self.cars:
-            car.forward(CAR_MOVE_DISTANCE)
+            car.forward(self.move_distance)
+
+    def increase_speed(self):
+        self.move_distance += CAR_MOVE_INCREMENT
+
+    def reset_cars(self):
+        for car in self.cars:
+            car.visible(0)
--- day_023/game.py
+++ day_023/game.py
@@ -4,7 +4,7 @@ from time import sleep
 from player import Player
 from car_manager import CarManager
 from scoreboard import ScoreBoard
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, FINISH_LINE
 
 
 screen = Screen()
@@ -24,11 +24,18 @@ is_game_on = True
 while is_game_on:
     screen.update()
     sleep(cars.move_speed)
-
+    cars.create_car()
     cars.move()
 
-    if player.ycor() == SCREEN_HEIGHT/2:
+    # detect collision with cars
+    for car in cars.cars:
+        if car.distance(player) < 20:
+            score.game_over()
+            is_game_on = False
+
+    if player.ycor() >= FINISH_LINE:
         player.reset_position()
         score.increase_score()
+        cars.increase_speed()
 
 screen.exitonclick()
